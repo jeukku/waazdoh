38222b16cc1eb651bca5812f63025424b9df3300
juuso <vilmunju@TIKE-J47Z0P1.mmmtike.fi>
2012-11-01 14:27:17 +0200

    Version changed.

--- waazdoh.common/src/main/java/waazdoh/CMusic.java
+++ waazdoh.common/src/main/java/waazdoh/CMusic.java
@@ -1,7 +1,7 @@
 package waazdoh;
 
 public class CMusic {
-	public final static String version = "0.2.0";
+	public final static String version = "0.2.1";
 	public static final int DEFAULT_SAMPLERATE = 44100;
 	public static final float MAX_RESOLUTION = 0.001f;
 }
