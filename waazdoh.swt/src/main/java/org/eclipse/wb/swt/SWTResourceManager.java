dcd4e647a799c68ca4e66b4dd441ef8415609c16
Juuso Vilmunen <juuso@vilmunen.net>
2013-04-23 19:20:18 +0300

    GPL Copyright.

--- waazdoh.swt/src/main/java/org/eclipse/wb/swt/SWTResourceManager.java
+++ waazdoh.swt/src/main/java/org/eclipse/wb/swt/SWTResourceManager.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2011 Google, Inc.
+ * Copyright (c) 2013 Juuso Vilmunen.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
+ * http://www.gnu.org/licenses/gpl.html
+ * 
  * Contributors:
- *    Google, Inc. - initial API and implementation
- *******************************************************************************/
+ *     Juuso Vilmunen - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.wb.swt;
 
 import java.io.FileInputStream;
